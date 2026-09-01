632a8643cc3389a86dcea68abdc4e47f3ccec6a4
Saran2416 <sarithasaran875@gmail.com>
2026-09-01 16:00:07 +0530

    new code

--- practice.c
+++ practice.c
@@ -2,5 +2,16 @@
 
 int main()
 {
-    printf("Hello World");
+    char name[20];
+    int age;
+
+    printf("Enter your name: ");
+    fgets(name, sizeof(name), stdin);
+
+    printf("Enter your age: ");
+    scanf("%d", &age);
+
+    printf("Hi %sYou are %d years old.\n", name, age);
+
+    return 0;
 }
